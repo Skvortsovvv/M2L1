d1c952ec9a455a7e0e9a2b0023827b98d16ce90a
Skvortsovvv <vladimirs001@yandex.ru>
2021-10-20 17:01:11 +0300

    '16'

--- C.py
+++ C.py
@@ -5,6 +5,7 @@ class Heap:
 
     def __init__(self):
         self.peaks = []
+        self.keys = {} # мапа из ключей и индексов для получения за константу
 
     def heapify(self, index):
         left = 2*index
@@ -29,14 +30,12 @@ class Heap:
         return None
 
     def set(self, key, value):
-        index = self.find(key)
-        if index is not None:
-            self.peaks[index][1] = value
-            return
+        self.peaks[key][1] = value
 
     def add(self, key, value):
         self.peaks.append((key, value))
         index = len(self.peaks) - 1
+        self.keys[key] = index
         while (index > 0) and (self.peaks[int(index/2)][0] > self.peaks[index][0]):
             self.peaks[index], self.peaks[int(index/2)] = self.peaks[int(index/2)], self.peaks[index]
             index = int(index/2)
@@ -45,47 +44,45 @@ class Heap:
         print(self.peaks[0][0], 0, self.peaks[0][1])
 
     def max(self):
-        a = 0
-        size1 = size2 = len(self.peaks)
-        while size1 > 1:
-            size1 = int(size1/2)
-            a += 1
-        index = pow(2, a)
-        index -= 1
-        maximum = None
-        for i in range(index, size2):
-            if maximum is None:
-                maximum = i
-            elif self.peaks[i][0] > self.peaks[maximum][0]:
-                maximum = i
-        print(self.peaks[maximum][0], maximum, self.peaks[maximum][1])
+        index_max = max(self.keys, key=self.keys.get)
+        print(self.peaks[index_max][0], index_max, self.peaks[index_max][1])
 
     def extract(self):
         print(self.peaks[0][0], self.peaks[0][1])
         index = len(self.peaks)-1
+        key = self.peaks[0][0]
         self.peaks[0] = self.peaks[index]
         del self.peaks[index:index+1]
+        del self.keys[key]
         self.heapify(0)
 
-    def delete(self, index):
-        self.peaks[index] = self.peaks[len(self.peaks)-1]
-        self.heapify(index)
+    def delete(self, key):
+        if key == self.peaks[len(self.peaks)-1][0]:
+            index = len(self.peaks) - 1
+            del self.peaks[index:index+1]
+        else:
+            index = self.keys[key][1]
+            self.peaks[0] = self.peaks[index]
+            del self.peaks[index:index+1]
+            del self.keys[key]
+            self.heapify(index)
+
 
     def print(self):
-        counter = 2
         if len(self.peaks) == 0:
             print('_')
             return
+        counter = 2
         for i in range(0, len(self.peaks)):
             if i == 0:
-                print('[' + self.peaks[i][0] + ' ' + self.peaks[i][1] + ']')
+                print('[' + str(self.peaks[i][0]) + ' ' + self.peaks[i][1] + ']')
             else:
                 if i % 2 == 0:
-                    sys.stdout.write('[' + self.peaks[i][0] + ' '
-                                     + self.peaks[i][1] + ' ' + self.peaks[int(i/2)][0] + ']')
+                    sys.stdout.write('[' + str(self.peaks[i][0]) + ' '
+                                     + self.peaks[i][1] + ' ' + str(self.peaks[int(i/2)][0]) + ']')
                 else:
-                    sys.stdout.write('[' + self.peaks[i][0] + ' '
-                                     + self.peaks[i][1] + ' ' + self.peaks[int(i-1/2)][0] + ']')
+                    sys.stdout.write('[' + str(self.peaks[i][0]) + ' '
+                                     + self.peaks[i][1] + ' ' + str(self.peaks[int(i-1/2)][0]) + ']')
                 if i is not counter:
                     sys.stdout.write(' ')
                 else:
@@ -103,20 +100,65 @@ class Heap:
         sys.stdout.write('_\n')
 
 
-
 def process(heap: Heap, command):
     if command == ' ':
         return
-    if re.fullmatch(r'add [-]?[\d+]+ [^\s+]+', command):
+    if re.fullmatch(r'add [-]?[\d+]+ [^\s+]+', command) is not None:
         pos1 = text[4:].find(' ')
         key = int(text[4:pos1 + 4])
         index = heap.find(key)
-        if index is not None:
+        if index is None:
             value = command[pos1+5:]
             heap.add(key, value)
         else:
             print('error')
-
+        return
+    if re.fullmatch(r"set [-]?[\d+]+ [^\s+]+", text) is not None:
+        pos1 = text[4:].find(' ')
+        key = int(text[4:pos1 + 4])
+        index = heap.find(key)
+        if index is not None:
+            value = text[pos1 + 5:]
+            heap.set(key, value)
+        else:
+            print('error')
+        return
+    if re.fullmatch(r'delete [-]?[\d+]+', text) is not None:
+        key = int(text[7:])
+        if heap.find(key) is not None:
+            heap.delete(key)
+        else:
+            print('error')
+        return
+    if re.fullmatch(r'search [-]?[\d+]+', text) is not None:
+        key = int(text[7:])
+        index = heap.find(key)
+        if index is not None:
+            print(1, index, heap.peaks[index][1])
+        else:
+            print(0)
+        return
+    if re.fullmatch(r'min', text) is not None:
+        if len(heap.keys) != 0:
+            heap.min()
+        else:
+            print('error')
+        return
+    if re.fullmatch(r'max', text) is not None:
+        if len(heap.keys) != 0:
+            heap.max()
+        else:
+            print('error')
+        return
+    if re.fullmatch(r'extract', text) is not None:
+        if len(heap.keys) != 0:
+            heap.extract()
+        else:
+            print('error')
+        return
+    if re.fullmatch(r'print', text) is not None:
+        heap.print()
+        return
 
 
 if __name__ == '__main__':
@@ -124,8 +166,8 @@ if __name__ == '__main__':
     min_heap = Heap()
 
     while True:
-        text = input()
-        process(min_heap, text)
-
-    pass
-
+        try:
+            text = input()
+            process(min_heap, text)
+        except EOFError:
+            break
